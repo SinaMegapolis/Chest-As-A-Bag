dd2c25f1fe188c15155820e1cf9966763228e2ba
SinaMegapolis <sinasaffari6@gmail.com>
2018-01-09 12:15:01 +0330

    just playing with code
    added a cancer code for rendering at TileEntityChestAsABagRenderer

--- src/main/java/net/sinamegapolis/caab/caab_code/tileentities/TileEntityChestAsABagRenderer.java
+++ src/main/java/net/sinamegapolis/caab/caab_code/tileentities/TileEntityChestAsABagRenderer.java
@@ -1,6 +1,14 @@
 package net.sinamegapolis.caab.caab_code.tileentities;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.sinamegapolis.caab.caab_code.blocks.BlockChestAsABag;
+import net.sinamegapolis.caab.init.ModRegistry;
+import net.sinamegapolis.caab.utill.CustomModelRegistry;
 
 /**
  * Created by m on 12/23/2017.
@@ -9,6 +17,10 @@ public class TileEntityChestAsABagRenderer extends TileEntitySpecialRenderer<Til
 
     @Override
     public void render(TileEntityChestAsABag te, double x, double y, double z, float partialTicks, int destroyStage, float alpha){
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(x,y,z);
 
+
+        GlStateManager.popMatrix();
     }
 }
